94a0c75d899596e9ae55a146c0e6605b62e35fe8
LanGuoguo <wangguoguo.lan@gmail.com>
2017-05-14 16:24:12 +0800

    chore: tag template

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -77,3 +77,9 @@ exports.getStoreBySlug = async (req, res, next) => {
   if (!store) return next();
   res.render('store', { title: store.name, store });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagList();
+  const tag = req.params.tag;
+  res.render('tag', { title: 'Tags', tags, tag });
+};
